158314531bde9eca7335d24d12b150b2fff5c9cf
chenqingwu <2524820850@qq.com>
2018-08-16 16:23:49 +0800

    对景点信息的查询

--- src/main/java/com/dyhc/sdglgroundconnection/service/ScenicspotService.java
+++ src/main/java/com/dyhc/sdglgroundconnection/service/ScenicspotService.java
@@ -1,8 +1,12 @@
 package com.dyhc.sdglgroundconnection.service;
 
 import com.dyhc.sdglgroundconnection.mapper.CommonMapper;
+import com.dyhc.sdglgroundconnection.pojo.Scenicspot;
+import com.github.pagehelper.PageInfo;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * this class by created wuyongfei on 2018/6/5 13:50
@@ -10,4 +14,20 @@ import org.springframework.stereotype.Component;
  *
  **/
 public interface ScenicspotService {
+
+    /**
+     * 根据名称、地址、等级状态查询景点
+     * @param typeId
+     * @return
+     */
+    PageInfo<Scenicspot> listScenicspots(Integer typeId, String scenicSpotName, String scenicSpotAddress, Integer whetherDel, Integer pageNo, Integer pageSize) throws Exception;
+
+
+
+    /**
+     * 根据父景点编号查询景点名称
+     * @param parentId
+     * @return
+     */
+    PageInfo<Scenicspot> getScenicspotByParentId(Integer parentId);
 }
--- src/main/java/com/dyhc/sdglgroundconnection/service/impl/ScenicspotServiceImpl.java
+++ src/main/java/com/dyhc/sdglgroundconnection/service/impl/ScenicspotServiceImpl.java
@@ -1,9 +1,15 @@
 package com.dyhc.sdglgroundconnection.service.impl;
 
 import com.dyhc.sdglgroundconnection.mapper.ScenicspotMapper;
+import com.dyhc.sdglgroundconnection.pojo.Scenicspot;
 import com.dyhc.sdglgroundconnection.service.ScenicspotService;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * this class by created wuyongfei on 2018/6/5 13:50
@@ -14,4 +20,19 @@ public class ScenicspotServiceImpl implements ScenicspotService {
 
     @Autowired
     private ScenicspotMapper scenicspotMapper;
+
+    @Override
+    public PageInfo<Scenicspot> listScenicspots(Integer typeId, String scenicSpotName, String scenicSpotAddress, Integer whetherDel, Integer pageNo, Integer pageSize) throws Exception {
+        PageHelper.startPage(pageNo, pageSize, true);
+        List<Scenicspot> list1=scenicspotMapper.listScenicspots(typeId,scenicSpotName,scenicSpotAddress,whetherDel);
+        PageInfo<Scenicspot> pageInfo = new PageInfo<>(list1);
+        return pageInfo;
+    }
+
+    @Override
+    public PageInfo<Scenicspot> getScenicspotByParentId(Integer parentId) {
+        List<Scenicspot> list1=scenicspotMapper.getScenicspotByParentId(parentId);
+        PageInfo<Scenicspot> pageInfo=new PageInfo<Scenicspot>(scenicspotMapper.getScenicspotByParentId(parentId));
+        return pageInfo;
+    }
 }
