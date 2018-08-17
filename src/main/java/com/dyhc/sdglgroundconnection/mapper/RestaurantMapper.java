c21db3aa59c7457e32d06029faf934585e102fb5
zhaoweiwei-1 <1061041247@qq.com>
2018-08-17 10:28:29 +0800

    111

--- src/main/java/com/dyhc/sdglgroundconnection/mapper/RestaurantMapper.java
+++ src/main/java/com/dyhc/sdglgroundconnection/mapper/RestaurantMapper.java
@@ -2,7 +2,10 @@ package com.dyhc.sdglgroundconnection.mapper;
 
 import com.dyhc.sdglgroundconnection.pojo.Restaurant;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * this class by created wuyongfei on 2018/6/5 13:50
@@ -11,4 +14,30 @@ import org.springframework.stereotype.Component;
 @Mapper
 @Component
 public interface RestaurantMapper extends CommonMapper<Restaurant>{
+    /**
+     * 赵伟伟
+     * 模糊查询饭店
+     * @param restaurantName
+     * @param restaurantAddress
+     * @param whetherDel
+     * @return
+     */
+    List<Restaurant> selectByEx(@Param("restaurantName") String restaurantName,@Param("restaurantAddress") String restaurantAddress,@Param("whetherDel") Integer whetherDel);
+
+    /**
+     * 赵伟伟
+     * 按照id删除饭店
+     * @param restaurantId
+     * @return
+     */
+    int delRestaurantById(@Param("restaurantId") int restaurantId) throws Exception;
+
+
+    /**
+     * 赵伟伟
+     * 修改饭店
+     * @param restaurant
+     * @return
+     */
+    int updateRestaurantById(Restaurant restaurant);
 }
