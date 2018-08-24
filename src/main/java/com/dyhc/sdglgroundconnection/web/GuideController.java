a4f9e708af1d6cdc902727a70212135fd8756d33
chenqingwu <2524820850@qq.com>
2018-08-24 09:07:42 +0800

    微信小程序

--- src/main/java/com/dyhc/sdglgroundconnection/web/GuideController.java
+++ src/main/java/com/dyhc/sdglgroundconnection/web/GuideController.java
@@ -1,7 +1,6 @@
 package com.dyhc.sdglgroundconnection.web;
 
 import com.alibaba.fastjson.JSON;
-import com.dyhc.sdglgroundconnection.pojo.AccountType;
 import com.dyhc.sdglgroundconnection.pojo.Guide;
 import com.dyhc.sdglgroundconnection.service.GuideService;
 import com.dyhc.sdglgroundconnection.utils.GuideUtils;
@@ -11,12 +10,11 @@ import com.github.pagehelper.PageInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
+
 
 /**
  * this class by created wuyongfei on 2018/6/5 13:50
@@ -129,6 +127,9 @@ public class GuideController{
             return ReponseResult.err("登录失败");
         }
     }
+
+
+
 
 
 }
--- src/main/java/com/dyhc/sdglgroundconnection/web/LogController.java
+++ src/main/java/com/dyhc/sdglgroundconnection/web/LogController.java
@@ -1,10 +1,14 @@
 package com.dyhc.sdglgroundconnection.web;
 
+import com.dyhc.sdglgroundconnection.pojo.Log;
 import com.dyhc.sdglgroundconnection.service.LogService;
+import com.dyhc.sdglgroundconnection.utils.LogNotes;
+import com.dyhc.sdglgroundconnection.utils.ReponseResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
  * 日志 控制层
  **/
 @RestController
+@RequestMapping("log")
 public class LogController  {
 
     // 日志对象
@@ -19,4 +24,30 @@ public class LogController  {
 
     @Autowired
     private LogService logService;
+
+    /**
+     * 添加带团日志
+     * @param log
+     * @return
+     */
+    @RequestMapping("insertLog")
+    @LogNotes(operationType="日志",content="添加")
+    public ReponseResult insertLog(Log log){
+        try {
+            Integer result=logService.inserLog(log);
+            ReponseResult data=null;
+            if (result==1){
+                logger.info("Mothod:insertLog  上传日志成功");
+                data=ReponseResult.ok(result,"上传日志成功");
+            }else {
+                logger.info("Mothod:insertLog  上传日志失败");
+                data=ReponseResult.ok(result,"上传日志失败");
+            }
+            return data;
+        }catch (Exception e){
+            e.printStackTrace();
+            logger.error("Mothod:insertLog  上传日志失败");
+            return ReponseResult.err("上传日志失败");
+        }
+    }
 }
