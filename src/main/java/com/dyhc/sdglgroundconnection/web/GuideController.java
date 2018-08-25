8169e678aab393ad4d72cc8b7b4f439e75c8c969
zhangfeng6 <15581702894@163.com>
2018-08-25 18:19:21 +0800

    Merge branch 'master' of https://github.com/zhangfeng6/sdglgroundconnection
    # Conflicts:
    #	src/main/resources/templates/countAdjust/index/count-Adjust-Index.html

--- src/main/java/com/dyhc/sdglgroundconnection/web/GuideController.java
+++ src/main/java/com/dyhc/sdglgroundconnection/web/GuideController.java
@@ -1,9 +1,12 @@
 package com.dyhc.sdglgroundconnection.web;
 
+import com.alibaba.fastjson.JSON;
 import com.dyhc.sdglgroundconnection.pojo.Guide;
 import com.dyhc.sdglgroundconnection.service.GuideService;
 import com.dyhc.sdglgroundconnection.utils.LogNotes;
+import com.dyhc.sdglgroundconnection.utils.MD5;
 import com.dyhc.sdglgroundconnection.utils.ReponseResult;
+import com.dyhc.sdglgroundconnection.utils.WechatFileUploadUtil;
 import com.github.pagehelper.PageInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,8 +14,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.multipart.MultipartFile;
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
+import java.util.Vector;
+
 
 /**
  * this class by created wuyongfei on 2018/6/5 13:50
